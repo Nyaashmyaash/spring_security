d414860d1f53932468557f8cebe5870adef712c7
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-10 05:13:41 +0700

    add changes

---
+++ src/main/java/com/nyash/spring/security/configuration/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.nyash.spring.security.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("employee").password("employee").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("hr").password("hr").roles("HR"))
+                .withUser(userBuilder.username("admin").password("admin").roles("MANAGER", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "MANAGER", "HR")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
+    }
+}
